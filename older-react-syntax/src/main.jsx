97114b5b75a2777e710bfdbfe8059b6e410f8588
Sia Zhong Qing <starsia93@gmail.com>
2026-07-28 01:15:59 +0800

    Associate App component with root id

--- older-react-syntax/src/main.jsx
+++ older-react-syntax/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import ReactDOM from "react-dom";
+// import registerServiceWorker from "./registerServiceWorker";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById("root"));
+// registerServiceWorker();
+
+// New versions
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
